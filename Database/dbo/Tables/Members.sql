d18718d1e31c6c496f33cbb1fac4f1d6e4e02477
Jay Creasy <jay.creasy@eurosafeuk.co.uk>
2018-11-13 16:11:37 +0000

    added db schema to project

---
+++ Database/dbo/Tables/Members.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Members] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [IsDeleted] BIT            NOT NULL,
+    [Forename]  NVARCHAR (MAX) NULL,
+    [Surname]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Members] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Database/dbo/Tables/Products.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [IsDeleted] BIT            NOT NULL,
+    [Name]      NVARCHAR (MAX) NULL,
+    [Cost]      REAL           NULL,
+    [MemberId]  INT            NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Products_Members_MemberId] FOREIGN KEY ([MemberId]) REFERENCES [dbo].[Members] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Products_MemberId]
+    ON [dbo].[Products]([MemberId] ASC);
+
